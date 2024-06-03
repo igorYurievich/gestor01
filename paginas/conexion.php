bec048b9009216a90cac6936af6aae9f667f14d0
IgorLipskiy <igoryurievich1@gmail.com>
2024-06-03 19:34:49 +0200

    Conexion fix

--- paginas/conexion.php
+++ paginas/conexion.php
@@ -1,15 +1,18 @@
 
 <?php
-$servername = "localhost"; 
-$username = "root";
-$password = ""; 
-$database = "gestor";
+$servername = "eu-cluster-west-01.k8s.cleardb.net";
+$username = "bf7be11ee1ae8b";
+$password = "9a3790ed6eb9dd0";
+$database = "heroku_40fe3ccaf131604";
+$port = 3306;
 
-// тестовый комментарий
-$conn = new mysqli($servername, $username, $password, $database);
+// Establecer conexión con la base de datos
+$conn = new mysqli($servername, $username, $password, $database, $port);
 
+// Verificar la conexión
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 
 ?>
