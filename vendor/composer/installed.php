41c23114f05baa483688b4dff92a37a7d42cb05d
IgorLipskiy <igoryurievich1@gmail.com>
2024-06-03 10:18:43 +0200

    Creación de un composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gestor/gestor',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c8372ab27de6eec9c92ca587414dee9b81185968',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gestor/gestor' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c8372ab27de6eec9c92ca587414dee9b81185968',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
