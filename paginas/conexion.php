e16bd6efde6ff0039acac194abf8c8b93662f26e
IgorLipskiy <igoryurievich1@gmail.com>
2024-06-03 11:52:28 +0200

    Coreccion del login

--- paginas/conexion.php
+++ paginas/conexion.php
@@ -1,21 +1,14 @@
 <?php
-$servername = "eu-cluster-west-01.k8s.cleardb.net";
-$username = "bf7be11ee1ae8b";
-$password = "9a3790ed6eb9dd0";
-$database = "heroku_40fe3ccaf131604";
-$port = 3306;
+$servername = "localhost"; 
+$username = "root";
+$password = ""; 
+$database = "gestor";
 
-// Установка соединения с базой данных
-$conn = new mysqli($servername, $username, $password, $database, $port);
+// тестовый комментарий
+$conn = new mysqli($servername, $username, $password, $database);
 
-// Проверка соединения
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Успешное подключение
-echo "Connected successfully";
-
-// Закрытие соединения (если оно больше не нужно)
-$conn->close();
 ?>
